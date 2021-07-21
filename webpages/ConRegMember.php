45e3bb64ae6bed7af06b79a88a4bc4f35d3e7083
James Shields <james@lostcarpark.com>
2021-07-21 01:13:12 +0000

    Forgot to save before last checkin.

--- webpages/ConRegMember.php
+++ webpages/ConRegMember.php
@@ -15,4 +15,18 @@ class ConRegMember
   {
     $connection = new mysqli(CONREG_DBHOSTNAME, CONREG_DBUSERID, CONREG_DBPASSWORD, CONREG_DBDB);
   }
+
+  /**
+   * Get the ConReg member ID for badge ID.
+   */
+  protected function getMid($badgeId)
+  {
+    $sql = "SELECT mid FROM conreg_zambia WHERE badgeid=?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('S', $badgeId);
+    $result = $stmt->get_result();
+    if ($result) {
+      return ($result->fetch_field());
+    }
+  }
 }
