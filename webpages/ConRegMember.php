ad56eb9a00cb64d379c008d6de5be4ebe5363494
James Shields <james@lostcarpark.com>
2021-07-22 23:58:13 +0000

    Update member on ConReg when edited.

--- webpages/ConRegMember.php
+++ webpages/ConRegMember.php
@@ -13,20 +13,33 @@ class ConRegMember
    */
   public function __construct()
   {
-    $connection = new mysqli(CONREG_DBHOSTNAME, CONREG_DBUSERID, CONREG_DBPASSWORD, CONREG_DBDB);
+    $this->connection = new mysqli(CONREG_DBHOSTNAME, CONREG_DBUSERID, CONREG_DBPASSWORD, CONREG_DBDB);
   }
 
   /**
    * Get the ConReg member ID for badge ID.
    */
-  protected function getMid($badgeId)
+  public function getMid($badgeId)
   {
     $sql = "SELECT mid FROM conreg_zambia WHERE badgeid=?";
-    $stmt = $connection->prepare($sql);
-    $stmt->bind_param('S', $badgeId);
+    $stmt = $this->connection->prepare($sql);
+    $stmt->bind_param('s', $badgeId);
+    $stmt->execute();
     $result = $stmt->get_result();
     if ($result) {
-      return ($result->fetch_field());
+      return ($result->fetch_object()->mid);
+    }
+    return NULL;
+  }
+  
+  public function updateMember($badgeId, $fields)
+  {
+    $mid = $this->getMid($badgeId);
+    if (!empty($mid)) {
+      $sql = "UPDATE conreg_members SET first_name=?, last_name=?, badge_name=?, phone=?, email=?, street=?, street2=?, city=?, county=?, postcode=? WHERE $mid=?";
+      $stmt = $this->connection->prepare($sql);
+      $stmt->bind_param('ssssssssssi', $fields['first_name'], $fields['last_name'], $fields['badge_name'], $fields['phone'], $fields['email'], $fields['street'], $fields['street2'], $fields['city'], $fields['county'], $fields['postcode'], $mid);
+      return $stmt->execute();
     }
   }
 }
--- webpages/SubmitMyContact.php
+++ webpages/SubmitMyContact.php
@@ -140,6 +140,22 @@ function update_participant($badgeid) {
             exit();
         }
 
+      // Update ConReg member before Zambia member details.
+      $conn = new ConRegMember();
+      $conn->updateMember('A0018', [
+        'first_name' => $fname,
+        'last_name' => $lname,
+        'badge_name' => $badgename,
+        'phone' => $phone,
+        'email' => $email,
+        'street' => $postaddress1,
+        'street2' => $postaddress2,
+        'city' => $postcity,
+        'county' => $poststate,
+        'postcode' => $postzip,
+      ]);
+
+
         $query = <<<EOD
 UPDATE CongoDumpHistory
     SET inactivatedts = CURRENT_TIMESTAMP, inactivatedbybadgeid = ?
